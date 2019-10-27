fa3a1613efc633b0e3c36d56ec4a834226202688
Abhinav Sharma <abhi18av@gmail.com>
2019-10-27 10:35:42 +0530

    practicing with vscode and ramda

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export * from './lib/async';
-export * from './lib/hash';
-export * from './lib/number';
+import R from 'ramda';
+
+console.log(
+
+//R.map(R.add(4), [1, 2, 3, 4])
+
+R.add(4)
+
+);
+
+
