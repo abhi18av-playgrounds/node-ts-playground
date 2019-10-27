124ce11a9b26ad78f14f4b314547e4970aa6dea5
Abhinav Sharma <abhi18av@gmail.com>
2019-10-27 11:43:41 +0530

    video-3 7:11

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import R from 'ramda';
 
+const myArr = [1, 2, 3, 4, 5];
+
+const myObj = {
+    'a': "A",
+    'b': "B",
+    'c': "C"
+};
+
+const add9 = R.add(9, R.__);
+const squareIt = (x) => Math.pow(x, 2);
+const printIt = (x, index) => console.log(x, index);
+const isNine = R.equals(9);
+const moreThanZero = R.gte(R.__, 0)
+
+
 console.log(
 
-//R.map(R.add(4), [1, 2, 3, 4])
-
-R.add(4)
-
+    R.all(moreThanZero, myArr)
 );
 
 
